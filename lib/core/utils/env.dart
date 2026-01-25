867e33e961c860b942cf4ecfa37b5f55ddcd45b8
Elberte Plinio <elberte.dev@gmail.com>
2026-01-25 17:30:16 -0300

    refactor(core): update Env class to use pure dart defines

--- lib/core/utils/env.dart
+++ lib/core/utils/env.dart
@@ -3,12 +3,10 @@ abstract class Env {
     'API_BASE_URL',
     defaultValue: 'http://localhost:8080/api/v1',
   );
-  static const String apiKey = String.fromEnvironment(
-    'API_KEY',
-    defaultValue: 'your_api_key',
+  static const String environment = String.fromEnvironment(
+    'ENVIRONMENT',
+    defaultValue: 'development',
   );
-  static const bool isProduction = bool.fromEnvironment(
-    'IS_PRODUCTION',
-    defaultValue: false,
-  );
+
+  static bool get isProduction => environment == 'production';
 }
