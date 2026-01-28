56b73f1f9a9e143c1a3aaa1eb937d64aeec5c44f
Elberte Plinio <elberte.dev@gmail.com>
2026-01-28 16:47:49 -0300

    feat(core): add webBaseUrl environment variable

--- lib/core/utils/env.dart
+++ lib/core/utils/env.dart
@@ -3,6 +3,10 @@ abstract class Env {
     'API_BASE_URL',
     defaultValue: 'http://localhost:8080/api/v1',
   );
+  static const String webBaseUrl = String.fromEnvironment(
+    'WEB_BASE_URL',
+    defaultValue: 'https://mygameslist.com',
+  );
   static const String environment = String.fromEnvironment(
     'ENVIRONMENT',
     defaultValue: 'development',
